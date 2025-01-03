3c085f63d629d94dd6a30be5f33a3654b3d0642f
Vanced Solutions <145443805+satnamsinghvanced@users.noreply.github.com>
2025-01-03 12:57:15 +0530

    update code

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 require("./db/connection");
 
 app.get("/api/testing", async (req, res) => {
-  res.send("Working 0.2");
+  res.send("Working 0.3");
 });
 
 // Authentication routes
