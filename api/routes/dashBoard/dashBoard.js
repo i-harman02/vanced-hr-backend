039c8a8fac2603bbab4ee99f5c53bc18e5f3407f
Vanced Solutions <145443805+satnamsinghvanced@users.noreply.github.com>
2025-01-06 13:11:33 +0530

    Update fixxed

--- api/routes/dashBoard/dashBoard.js
+++ api/routes/dashBoard/dashBoard.js
@@ -8,7 +8,7 @@ const auth = require('../../helpers/auth')
 
 router.get("/all-count",auth, async (req, res) => {
   try {
-    const employee = await Employee.find({});
+    const employee = await Employee.find({role: { $ne: "admin" },});
     const client = await Client.find({});
     const project = await Projects.find({});
     const totalProjects = project.length;
